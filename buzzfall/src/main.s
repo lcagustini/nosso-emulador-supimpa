7b5ea010555a95d5c7ea05f581ab7ac103754c58
Italo Nicola <italo.niccola@gmail.com>
2019-08-17 17:33:56 -0300

    Basic example with sprite and background working

--- buzzfall/src/main.s
+++ buzzfall/src/main.s
@@ -1,92 +1,214 @@
 .segment "HEADER"
-.byte "NES" ; signature
-.byte $1A   ; signature
-.byte $02   ; # of 16kb PRG-ROM banks
-.byte $01   ; # of 8kb VROM banks
-.byte $00   ; ROM control byte one
-.byte $00   ; ROM control byte two
-.byte $00   ; # of 8kb RAM banks
-.byte $00   ; reserved
+.byte "NES"                       ; signature
+.byte $1A                         ; signature
+.byte $02                         ; # of 16kb PRG-ROM banks
+.byte $01                         ; # of 8kb VROM banks
+.byte $00                         ; ROM control byte one
+.byte $00                         ; ROM control byte two
+.byte $00                         ; # of 8kb RAM banks
+.byte $00                         ; reserved
 
 .segment "VECTORS"
 .addr nmi
 .addr reset
 .addr irq
 
+.segment "ZEROPAGE"               ; is this really supposed to be used like this? what about rs = 1?
+bgPointerLo:  .byte 0             ; pointer variables are declared in RAM
+bgPointerHi:  .byte 0             ; low byte first, high byte immediately after
+counterLo:    .byte 0
+counterHi:    .byte 0
+
 .segment "CODE"
-
 reset:
-  sei        ; ignore IRQs
-  cld        ; disable decimal mode
+  sei                             ; disable IRQs
+  cld                             ; disable decimal mode
   ldx #$40
-  stx $4017  ; disable APU frame IRQ
-  ldx #$ff
-  txs        ; Set up stack
-  inx        ; now X = 0
-  stx $2000  ; disable NMI
-  stx $2001  ; disable rendering
-  stx $4010  ; disable DMC IRQs
+  stx $4017                       ; disable APU frame IRQ
+  ldx #$FF
+  txs                             ; Set up stack
+  inx                             ; now X = 0
+  stx $2000                       ; disable NMI
+  stx $2001                       ; disable rendering
+  stx $4010                       ; disable DMC IRQs
 
-; Optional (omitted):
-; Set up mapper and jmp to further init code here.
+                                  ; Optional (omitted):
+                                  ; Set up mapper and jmp to further init code here.
 
-; If the user presses Reset during vblank, the PPU may reset
-; with the vblank flag still true.  This has about a 1 in 13
-; chance of happening on NTSC or 2 in 9 on PAL.  Clear the
-; flag now so the @vblankwait1 loop sees an actual vblank.
+                                  ; If the user presses Reset during vblank, the PPU may reset
+                                  ; with the vblank flag still true.  This has about a 1 in 13
+                                  ; chance of happening on NTSC or 2 in 9 on PAL.  Clear the
+                                  ; flag now so the @vblankwait1 loop sees an actual vblank.
   bit $2002
 
-; First of two waits for vertical blank to make sure that the
-; PPU has stabilized
-vblankwait1:  
+vblankwait1:                      ; First wait for vblank to make sure PPU is ready
   bit $2002
   bpl vblankwait1
 
-; We now have about 30,000 cycles to burn before the PPU stabilizes.
-; One thing we can do with this time is put RAM in a known state.
-; Here we fill it with $00, which matches what (say) a C compiler
-; expects for BSS.  Conveniently, X is still 0.
-  txa
-clrmem:
-  sta $000,x
-  sta $100,x
-  sta $300,x
-  sta $400,x
-  sta $500,x
-  sta $600,x
-  sta $700,x  ; Remove this if you're storing reset-persistent data
-
-; We skipped $200,x on purpose.  Usually, RAM page 2 is used for the
-; display list to be copied to OAM.  OAM needs to be initialized to
-; $EF-$FF, not 0, or you'll get a bunch of garbage sprites at (0, 0).
-
+clrmem:                           ; Set up RAM before waiting for the second vblank
+  lda #$00
+  sta $0000, x
+  sta $0100, x
+  sta $0300, x
+  sta $0400, x
+  sta $0500, x
+  sta $0600, x
+  sta $0700, x
+  lda #$FE
+  sta $0200, x                    ; Shouldn't be zeroed, since that would mean X position is 0
+                                  ; Instead, we set the X poision to 0xFE, meaning it is offscreen
   inx
   bne clrmem
-
-; Other things you can do between vblank waits are set up audio
-; or set up other mapper registers.
-
-vblankwait2:
+   
+vblankwait2:                      ; Second wait for vblank, PPU is ready after this
   bit $2002
   bpl vblankwait2
 
-chngbkr:
-  lda #$3F
-  sta $2006 ;first we write the upper byte of the ppu adress we want to ;write to in this case $3F00
+
+
+  lda #<(background)
+  sta bgPointerLo
+  lda #>(background)
+  sta bgPointerHi
   lda #$00
-  sta $2006 ;now we write the lower byte #$00
-  lda #$2a ;value green = #$2a
-  sta $2007 ;ppudata
-  lda #%00001000
-  sta $2001 ;main screen turn on
+  sta counterLo                   ; put the loop counter into 16 bit variable
+  lda #$04
+  sta counterHi                   ; count = $0400 = 1KB, the whole screen at once including attributes
+LoadBackground:
+  lda $2002                       ; reset the latch to high
+  lda #$20
+  sta $2006                       ; first we write the upper byte of the ppu adress we want to ;write to in this case $3F00
+  lda #$00
+  sta $2006                       ; now we write the lower byte #$00
+  ldy #$00                        ; unused
+LoadBackgroundLoop:
+  lda (bgPointerLo), y
+  sta $2007
+  
+  ;;;;; INCREMENT 16-bit ;;;;;
+  lda bgPointerLo
+  clc
+  adc #$01
+  sta bgPointerLo
+  lda bgPointerHi
+  adc #$00
+  sta bgPointerHi
 
-loop:
-  jmp loop
+  ;;;;; DECREMENT 16-bit ;;;;;;
+  lda counterLo
+  sec
+  sbc #$01
+  sta counterLo
+  lda counterHi
+  sbc #$00
+  sta counterHi
 
-nmi:
+  lda counterLo
+  cmp #$00
+  bne LoadBackgroundLoop
+  lda counterHi
+  cmp #$00
+  bne LoadBackgroundLoop          ; if the loop counter isn't 0000, keep copying
+
+LoadAttribute:
+  ;lda $2002                      ; we don't need to reset the latch since it's already on high
+  lda #$23
+  sta $2006                       ; first we write the upper byte of the ppu adress we want to write to
+  lda #$C0
+  sta $2006                       ; now we write the lower byte #$00
+  ldx #$00
+LoadAttributeLoop:
+  lda attribute, x
+  sta $2007
+  inx
+  cpx #$8
+  bne LoadAttributeLoop
+
+LoadPalettes:
+  ;lda $2002                      ; we don't need to reset the latch since it's already on high
+  lda #$3F
+  sta $2006                       ; first we write the upper byte of the ppu adress we want to write to
+  lda #$00
+  sta $2006                       ; now we write the lower byte #$00
+  ldx #$00
+LoadPalettesLoop:
+  lda palette, x
+  sta $2007
+  inx
+  cpx #$20
+  bne LoadPalettesLoop
+
+  ; player 1
+  lda #$80
+  sta $0200                       ; Y
+  lda #$80
+  sta $0203                       ; X
+  lda #$00
+  sta $0201                       ; tile number = 1
+  lda #$00
+  sta $0202                       ; color = 0, no flipping
+
+  ; player 2
+  lda #$80
+  sta $0204                       ; Y
+  lda #$88
+  sta $0207                       ; X
+  lda #$01
+  sta $0205                       ; tile number = 1
+  lda #$00
+  sta $0206                       ; color = 0, no flipping
+
+  ; player 3                      
+  lda #$88
+  sta $0208                       ; Y
+  lda #$80
+  sta $020B                       ; X
+  lda #$02
+  sta $0209                       ; tile number = 1
+  lda #$00
+  sta $020A                       ; color = 0, no flipping
+
+  ; player 4                      
+  lda #$88
+  sta $021C                       ; Y
+  lda #$88
+  sta $021F                       ; X
+  lda #$03
+  sta $021D                       ; tile number = 1
+  lda #$00
+  sta $021E                       ; color = 0, no flipping
+  
+  lda #%10010000                  ; enable NMI, sprites from Pattern 0, background from Pattern 1
+  sta $2000
+
+  lda #%00011110                  ; enable sprites and background
+  sta $2001
+
+Forever:
+  jmp Forever                     ; jump back to Forever, infinite loop
+  
+
+nmi:                              ; VBLANK interrupt
+  lda #$00
+  sta $2003                       ; Why are the high and low so far apart????
+  lda #$02
+  sta $4014                       ; start dma
+
+  lda #$00                        ; Scroll position!! This is needed because using PPUADDR overwrites PPUSCROLL!!
+  sta $2005
+  sta $2005
   rti
+
 irq:
   rti
+ 
+;;;;;;;;;;;;;;
 
+.segment "GFX_DATA"
+.include "gfx.s"
+
+;;;;;;;;;;;;;;  
+  
 .segment "CHARS"
-.segment "TILES"
+.incbin "mario.chr"               ; includes 8KB graphics file from SMB1
+
