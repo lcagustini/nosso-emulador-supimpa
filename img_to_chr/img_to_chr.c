e9386f8d283260ed301ec86c9e51a867bff2575e
lcagustini <torresmon235@gmail.com>
2019-08-21 19:20:41 -0300

    New color difference model

--- img_to_chr/img_to_chr.c
+++ img_to_chr/img_to_chr.c
@@ -26,7 +26,48 @@ const int nes_palette[64] = {
   0xF8D878, 0xD8F878, 0xB8F8B8, 0xB8F8D8, 0x00FCFC, 0xF8D8F8, 0x000000, 0x000000,
 };
 
-float color_distance(int a, int b) {
+void rgb2lab(int ir, int ig, int ib, double ret[3]) {
+  double r = ir / 255.0;
+  double g = ig / 255.0;
+  double b = ib / 255.0;
+  double x, y, z;
+
+  r = (r > 0.04045) ? pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = (g > 0.04045) ? pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = (b > 0.04045) ? pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.00000;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+  x = (x > 0.008856) ? pow(x, 1/3.0) : (7.787 * x) + 16.0/116;
+  y = (y > 0.008856) ? pow(y, 1/3.0) : (7.787 * y) + 16.0/116;
+  z = (z > 0.008856) ? pow(z, 1/3.0) : (7.787 * z) + 16.0/116;
+
+  ret[0] = (116 * y) - 16;
+  ret[1] = 500 * (x - y);
+  ret[2] = 200 * (y - z);
+}
+
+double deltaE(double labA[3], double labB[3]) {
+  double deltaL = labA[0] - labB[0];
+  double deltaA = labA[1] - labB[1];
+  double deltaB = labA[2] - labB[2];
+  double c1 = sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+  double c2 = sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+  double deltaC = c1 - c2;
+  double deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+  deltaH = deltaH < 0 ? 0 : sqrt(deltaH);
+  double sc = 1.0 + 0.045 * c1;
+  double sh = 1.0 + 0.015 * c1;
+  double deltaLKlsl = deltaL / (1.0);
+  double deltaCkcsc = deltaC / (sc);
+  double deltaHkhsh = deltaH / (sh);
+  double i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+  return i < 0 ? sqrt(-i) : sqrt(i);
+}
+
+double color_distance(int a, int b) {
   int ar = (a >> 16) & 0xff;
   int ag = (a >>  8) & 0xff;
   int ab = (a >>  0) & 0xff;
@@ -35,11 +76,10 @@ float color_distance(int a, int b) {
   int bg = (b >>  8) & 0xff;
   int bb = (b >>  0) & 0xff;
 
-  int dist_r = ar - br;
-  int dist_g = ag - bg;
-  int dist_b = ab - bb;
-
-  return sqrt(dist_r * dist_r + dist_g * dist_g + dist_b * dist_b);
+  double alab[3], blab[3];
+  rgb2lab(ar, ag, ab, alab);
+  rgb2lab(br, bg, bb, blab);
+  return deltaE(blab, alab);
 }
 
 int tile_to_grid(unsigned char *data) {
@@ -50,10 +90,10 @@ int tile_to_grid(unsigned char *data) {
       int b = data[3*8*j + 3*i + 2];
       int color = (r << 16) | (g << 8) | (b << 0);
 
-      float min_dist = color_distance(color, nes_palette[0]);
+      double min_dist = color_distance(color, nes_palette[0]);
       int min_dist_index = 0;
       for (int j = 1; j < 64; j++) {
-        float candidate = color_distance(color, nes_palette[j]);
+        double candidate = color_distance(color, nes_palette[j]);
         if (candidate < min_dist) {
           min_dist = candidate;
           min_dist_index = j;
@@ -111,10 +151,10 @@ int main(int argc, char **argv) {
     num_colors = 1;
     int color = strtol(argv[3], NULL, 16);
 
-    float min_dist = color_distance(color, nes_palette[0]);
+    double min_dist = color_distance(color, nes_palette[0]);
     int min_dist_index = 0;
     for (int j = 1; j < 64; j++) {
-      float candidate = color_distance(color, nes_palette[j]);
+      double candidate = color_distance(color, nes_palette[j]);
       if (candidate < min_dist) {
         min_dist = candidate;
         min_dist_index = j;
