3e1de2d57ba651bcd19fd1494b38d87e8f044510
lcagustini <torresmon235@gmail.com>
2019-09-10 12:27:55 -0300

    Initial instructions

--- src/cpu.c
+++ src/cpu.c
@@ -1,5 +1,24 @@
 void doInstruction(uint8_t opcode) {
+  printf(COLOR_BLUE);
   switch (opcode) {
+    case 0x78: // sei
+      cpu.rb.p |= 0b100;
+      break;
+    case 0x8E: // srx abs
+      writeCPUByte(getInstructionAddrs(), cpu.rb.x);
+      break;
+    case 0x9A: // txs
+      cpu.rb.sp = cpu.rb.x;
+      break;
+    case 0xA2: // ldx #imm
+      cpu.rb.x = getInstructionByte();
+      break;
+    case 0xD8: // cld
+      cpu.rb.p &= ~0b1000;
+      break;
+    case 0xE8: // inx
+      cpu.rb.x++;
+      break;
     default:
       fprintf(stderr, COLOR_RED "Unimplemented OP Code: 0x%X\n", opcode);
       exit(1);
